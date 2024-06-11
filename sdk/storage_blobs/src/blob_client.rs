05fd8ea370cc9e20f6b737f15ca7a9f6883ebf5e
Vincent Tran <vincenttran@microsoft.com>
2024-06-10 20:04:55 -0700

    Base client, BlobClient only

--- sdk/storage_blobs/src/blob_client.rs
+++ sdk/storage_blobs/src/blob_client.rs
@@ -1,17 +1,12 @@
-use azure_core::{
-    auth::TokenCredential, policies::BearerTokenCredentialPolicy, ClientOptions, Context, Header,
-    Method, Pipeline, Policy, Request, Response, Url,
-};
-use azure_identity::create_credential;
-use std::sync::Arc;
+use crate::BaseClient;
+use azure_core::{Context, Method, Pipeline, Request, Response, Url};
 
 pub struct BlobClient<'a> {
     // At the moment, we aren't really using this for anything so comes up as "dead fields"
     account_name: &'a str,
-    credential: Arc<dyn TokenCredential>,
     container_name: &'a str,
     blob_name: &'a str,
-    url: Url,
+    blob_url: Url,
     pipeline: Pipeline,
 }
 
@@ -22,54 +17,26 @@ impl<'a> BlobClient<'a> {
         container_name: &'a str,
         blob_name: &'a str,
     ) -> Self {
-        // Create Respective Authentication Pipeline
-
-        // OAuth Pipeline Policy
-        println!("Auth type chosen, Oauth, {}", credential);
-        let credential = create_credential().expect("Failed for some reason?");
-        let oauth_token_policy = BearerTokenCredentialPolicy::new(
-            credential.clone(),
-            &["https://storage.azure.com/.default"],
-        );
-
-        // Runner Pipeline
-        let runner_pipeline = Pipeline::new(
-            option_env!("CARGO_PKG_NAME"),
-            option_env!("CARGO_PKG_VERSION"),
-            ClientOptions::default(),
-            vec![Arc::new(oauth_token_policy) as Arc<dyn Policy>],
-            Vec::new(),
-        );
+        // Create Base Client
+        let base_client = BaseClient::new(account_name, "blob", credential);
 
         // Build URL from Input (No validation atm)
-        let blob_url = "https://".to_owned()
-            + &account_name
-            + ".blob.core.windows.net/"
-            + &container_name
-            + "/"
-            + &blob_name;
+        let blob_url = base_client.base_url.to_string() + &container_name + "/" + &blob_name;
 
         // Build our BlobClient
         Self {
             account_name: account_name,
-            credential: credential,
             container_name: container_name,
             blob_name: blob_name,
-            url: Url::parse(&blob_url).expect("Something went wrong with URL parsing!"),
-            pipeline: runner_pipeline,
+            blob_url: Url::parse(&blob_url).expect("Something went wrong with URL parsing!"),
+            pipeline: base_client.pipeline,
         }
-    }
-
-    // For now, this will handle the x-ms-version issue
-    fn finalize_request(mut request: Request) -> Request {
-        request.insert_header("x-ms-version", "2023-11-03");
-        request
     }
 
     pub async fn download_blob(&self) -> String {
         // Build the download request itself
-        let mut request = Request::new(self.url.to_owned(), Method::Get); // This is technically cloning
-        request = BlobClient::finalize_request(request);
+        let mut request = Request::new(self.blob_url.to_owned(), Method::Get); // This is technically cloning
+        request = BaseClient::finalize_request(request);
 
         // Send the request
         let response = self.pipeline.send(&(Context::new()), &mut request).await;
@@ -85,8 +52,8 @@ impl<'a> BlobClient<'a> {
 
     pub async fn get_blob_properties(&self) -> Response {
         // Build the get properties request itself
-        let mut request = Request::new(self.url.to_owned(), Method::Head); // This is technically cloning
-        request = BlobClient::finalize_request(request);
+        let mut request = Request::new(self.blob_url.to_owned(), Method::Head); // This is technically cloning
+        request = BaseClient::finalize_request(request);
 
         // Send the request
         let response = self.pipeline.send(&(Context::new()), &mut request).await;
