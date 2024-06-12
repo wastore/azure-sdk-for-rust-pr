cd41f40869e38e53d5a3fb187a2810258df17364
Vincent Tran <vincenttran@microsoft.com>
2024-06-12 16:23:01 -0700

    Base client trait idea

--- sdk/storage_blobs/src/blob_container_client.rs
+++ sdk/storage_blobs/src/blob_container_client.rs
@@ -1,33 +1,86 @@
-use std::marker::PhantomData;
+use std::sync::Arc;
 
-use azure_core::{error::HttpError, Pageable, Url};
+use azure_core::{auth::TokenCredential, Context, Method, Pipeline, Request, Response, Url};
 
-use crate::{units::*, BlobClient, BlobClientOptions};
+use crate::base_client::BaseClient;
 
-pub struct BlobContainerClient<T>
-where
-    T: AccountStructure,
-{
-    account_structure: PhantomData<T>,
-    endpoint: Url,
+pub struct BlobContainerClient {
+    account_name: String,
+    credential: Arc<dyn TokenCredential>,
+    container_name: String,
+    url: Url,
+    pipeline: Pipeline,
 }
 
-pub struct BlobItem {}
+// Even just this empty block will give us access to BaseClient's traits
+impl BaseClient for BlobContainerClient {}
 
-impl<T: AccountStructure> BlobContainerClient<T> {
-    pub fn new(endpoint: Url, options: &BlobClientOptions) -> BlobContainerClient<Unset> {
-        todo!()
+impl BlobContainerClient {
+    pub fn new(account_name: String, credential: String, container_name: String) -> Self {
+        // Build ContainerClient-specific URL
+        let container_url = BlobContainerClient::build_container_url(
+            &BlobContainerClient::build_url(&account_name, "blob"),
+            &container_name,
+        );
+
+        // Get Credential Object
+        let credential = BlobContainerClient::get_credential();
+
+        // Build our BlobContainerClient
+        Self {
+            account_name: account_name,
+            credential: credential.clone(), // Unsure if clone is the correct move here
+            container_name: container_name,
+            url: Url::parse(&container_url).expect("Something went wrong with URL parsing!"),
+            pipeline: BlobContainerClient::build_pipeline(credential),
+        }
     }
 
-    pub fn endpoint(&self) -> &Url {
-        &self.endpoint
+    // This will handle appending container name
+    fn build_container_url(base_url: &str, container_name: &str) -> String {
+        base_url.to_owned() + container_name + "/" + "?restype=container"
     }
 
-    // pub fn get_blob_client(&self, blob_name: &str) -> BlobClient<Unset, Unset> {
-    //     todo!()
-    // }
+    pub async fn get_container_properties(&self) -> Response {
+        // Build the get properties request itself
+        let mut request = Request::new(self.url.to_owned(), Method::Head); // This is technically cloning
+        BlobContainerClient::finalize_request(&mut request);
 
-    pub fn get_blobs(&self) -> Pageable<BlobItem, HttpError> {
-        todo!()
+        // Send the request
+        let response = self.pipeline.send(&(Context::new()), &mut request).await;
+        println!("Response headers: {:?}", response);
+
+        // Return the response headers
+        response.unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use azure_core::headers::HeaderName;
+
+    use crate::BlobContainerClient;
+
+    #[tokio::test]
+    async fn test_get_container_properties() {
+        // Create a Container Client
+        let my_blob_container_client = BlobContainerClient::new(
+            "vincenttranstock".to_string(),
+            "throwaway".to_string(),
+            "acontainer108f32e8".to_string(),
+        );
+
+        // Get response
+        let ret = my_blob_container_client.get_container_properties().await;
+        let (status_code, headers, response_body) = ret.deconstruct();
+
+        // Assert equality
+        assert_eq!(status_code, azure_core::StatusCode::Ok);
+        assert_eq!(
+            headers
+                .get_str(&HeaderName::from_static("content-length"))
+                .expect("Failed getting content-length header"),
+            "0"
+        )
     }
 }
